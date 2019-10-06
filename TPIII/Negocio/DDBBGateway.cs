035ac5e7a341c3d84008be9eeee6d2c9438960c9
Federico 'Magus' Rocca <frocca17@gmail.com>
2019-10-06 20:37:24 -0300

    Comento linea de conexión de ddbb para que cuando se compile fallé la compilación y se tenga que agregar la nueva conexión (Por corrección de TP)

--- TPIII/Negocio/DDBBGateway.cs
+++ TPIII/Negocio/DDBBGateway.cs
@@ -22,10 +22,10 @@ namespace Negocio
         /// </summary>
         public DDBBGateway()
         {
-            /// Desktop-PC
-            connectionString = "data source=DESKTOP-BA6HNP1\\SQLEXPRESS01; integrated security=sspi; initial catalog=TP_WEB;";
-            /// Notebook-PC
-           // connectionString = "data source=DESKTOP-SI2UFE1\\SQLEXPRESS; integrated security=sspi; initial catalog=TP_WEB;";
+            // ---------------------------- Desktop-PC ----------------------------
+            // connectionString = "data source=DESKTOP-BA6HNP1\\SQLEXPRESS01; integrated security=sspi; initial catalog=TP_WEB;";
+            // ---------------------------- Notebook-PC ----------------------------
+            // connectionString = "data source=DESKTOP-SI2UFE1\\SQLEXPRESS; integrated security=sspi; initial catalog=TP_WEB;";
 
             connection.ConnectionString = connectionString;
             command.Connection = connection;
